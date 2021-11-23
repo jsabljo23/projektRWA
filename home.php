5872246dba95995d66cda401086d014bc217d568
jsabljo23 <94869661+jsabljo23@users.noreply.github.com>
2021-11-23 09:53:29 +0100

    Add files via upload

---
+++ home.php
@@ -0,0 +1,76 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="css/main.css">
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Naslov stranice</title>
+</head>
+<body class="backgrnd">
+    <div class="container-fluid main_container d-flex">
+        <div class="row flex-fill">
+
+            <div class="col-md-12 topMar">
+                <div class="col-md-1 topInfo">
+                    <h3>Naslov </h3>
+                </div>
+            </div>
+
+            <div class="col-md-6 infoTable">
+                <div class="row">
+                    <div class="col-md-6 leftInfo">
+                        <h5 class="infoTittle">Trazim automobil</h5>
+                        <hr class="bg-primary">
+                        <p class="textInfo">Pritiskom na link pretrazite nasu bazu u kojoj se nalaze vise od 100 automobila</p>
+                        <a href="#" class="linkD"><div class="col-md-12 searchCar text-center">
+                            <br>
+                            <br>
+                            <div class="hoverText"><p>Trazi automobil</p></div>
+                        </div></a>
+                    </div>
+
+                    <div class="col-md-6 rightInfo">
+                        <h5 class="infoTittle">Trazim komponente</h5>
+                        <hr class="bg-primary">
+                        <p class="textInfo">Pritiskom na link pretrazite nasu bazu u kojoj se nalaze vise od 1000 dijelova</p> 
+                        <a href="#" class="linkD"><div class="col-md-12 searchPart text-center">
+                            <br>
+                            <br>
+                            <div class="hoverText"><p>Trazi komponente</p></div>
+                        </div></a>
+                    </div>
+
+                    <div class="col-md-12 botInfo">
+                        <p>Niste korisnik? Prijavite se i otkrijte dodatne mogucnosti</p>
+                        <button type="button" class="btn btn-outline-light">Prijavi se</button>
+                    </div>
+                </div>
+            </div>
+    
+            <div class="col-md-12 botDiv">
+                    <div class="col-md-6 leftBot">
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Exercitationem, 
+                            voluptate? Incidunt repellendus dolores tempore quae ea! Repudiandae, minus 
+                            totam quos, hic blanditiis recusandae perferendis doloribus illum, inventore quo laborum ipsam.
+                        </p>
+                    </div>
+
+                    <div class="col-md-6 rightBot">
+                        <p>Mogucnosti stranice</p>
+                        <ul>
+                            <li>Pretraga automobila</li>
+                            <li>Pretraga komponenata automobila</li>
+                            <li>Mogucnost prodavanja automobila.<a href="#"><sup>Saznaj vise</sup></a></li>
+                            <li>Premium profil.<a href="#"><sup>Saznaj vise</sup></a></li>
+                            <li>Forum za rjesavanje problema</li>
+                            <li>Pregled rezultata sa ostalih stranica(Olx.ba,Ebay)</li>
+                        </ul>
+                    </div>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
